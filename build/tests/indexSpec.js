48e87a3c62dc5931227226dbaee5039b5e70d39a
kellywoldseth <kelly.woldseth@gmail.com>
2022-04-15 12:43:12 -0700

    having trouble with jasmine

---
+++ build/tests/indexSpec.js
@@ -0,0 +1,10 @@
+import add from '../index';
+console.log('test');
+
+it('adds 2 nums', function () {
+    var val = add(3, 4);
+    expect(val).toBe(5);
+});
+it('adds 0 + 0 falsy', function () {
+    expect(add(0, 0)).toBeFalsy();
+});
---
+++ src/tests/indexSpec.ts
@@ -0,0 +1,10 @@
+import add from '../index';
+
+it('adds 2 nums', () => {
+  const val = add(3, 4);
+  expect(val).toBe(5);
+});
+
+it('adds 0 + 0 falsy', () => {
+  expect(add(0, 0)).toBeFalsy();
+});
